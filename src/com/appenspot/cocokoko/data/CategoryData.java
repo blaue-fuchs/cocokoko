7b6f8351f6fdbe4708cb1a5f3dad8ea8d3a2ad5b
hamasakit@gmail.com <hamasakit@gmail.com@bd7e7284-7e5f-11de-b59e-c5e9133e0c40>
2009-08-08 17:47:51 +0000

    make date fields

--- src/com/appenspot/cocokoko/data/CategoryData.java
+++ src/com/appenspot/cocokoko/data/CategoryData.java
@@ -1,5 +1,76 @@
 package com.appenspot.cocokoko.data;
+/****
+ * カテゴリデータ
+ */
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.IdentityType;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
 
+@PersistenceCapable(identityType = IdentityType.APPLICATION)
 public class CategoryData {
+    @PrimaryKey
+    @Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+    private Long id;
 
+    /**
+     * カテゴリID
+     */
+	@Persistent
+	private Integer categoryID;
+	
+	/**
+	 * カテゴリ名
+	 */
+	@Persistent
+	private String categoryNm;
+	
+	/**
+	 * 表示アイコン
+	 */
+	@Persistent
+	private String iconImage;
+
+	public CategoryData() {
+		super();
+	}
+
+	public CategoryData( Integer categoryID, String categoryNm,
+			String iconImage) {
+		super();
+		this.categoryID = categoryID;
+		this.categoryNm = categoryNm;
+		this.iconImage = iconImage;
+	}
+
+	public Integer getCategoryID() {
+		return categoryID;
+	}
+
+	public void setCategoryID(Integer categoryID) {
+		this.categoryID = categoryID;
+	}
+
+	public String getCategoryNm() {
+		return categoryNm;
+	}
+
+	public void setCategoryNm(String categoryNm) {
+		this.categoryNm = categoryNm;
+	}
+
+	public String getIconImage() {
+		return iconImage;
+	}
+
+	public void setIconImage(String iconImage) {
+		this.iconImage = iconImage;
+	}
+
+	public Long getId() {
+		return id;
+	}
+	
+	
 }
